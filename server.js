b46210ee97f3bcdb372a4cfecb593a4dae16dd93
tishraq28 <ish.tan.v.a.ex@gmail.com>
2023-05-08 23:34:18 +0600

    an one

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 let path = require('path')
-path = path.join(__dirname, '..', 'public', 'main.html')
+path = path.join(__dirname, 'public', 'main.html')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(path))
